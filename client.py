46c1052e6b33a38c92dfef8add613d070cd59c81
Abdelmalek Ihdene <abdelmalek.ihdene@gmail.com>
2022-10-09 18:07:50 -0400

    Initial commit

---
+++ client.py
@@ -0,0 +1,57 @@
+import pyinputplus as pyip
+import requests
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import Parse as JSONParse
+
+import types_pb2 as pbtypes
+
+print("//////////////////////////////////////////////////////")
+print("Welcome to the RFW Request Generator & Client [RFWRGC]")
+print("By Abdelmalek Ihdene & Jonathan Perlman.")
+print("//////////////////////////////////////////////////////")
+print("The client will:")
+print("1. Prompt the user with a set of inquiries.")
+print("2. Generate an HTTP request from the resulting answers.")
+print("3. Send that request over to the server.")
+print("4. Display the resulting RFD payload.")
+print("//////////////////////////////////////////////////////")
+
+def inputMenuWrapper(prompt, enum):
+  return pyip.inputMenu(prompt=prompt, choices=enum.keys())
+
+request = pbtypes.RFW()
+request.Id = pyip.inputInt("Enter the ID of the RFW:\n")
+request.benchmarkType = inputMenuWrapper("\nEnter benchmark type:\n", pbtypes.BenchmarkType)
+request.workloadMetric = inputMenuWrapper("\nEnter workload metric:\n", pbtypes.WorkloadMetric)
+request.sampleCount = pyip.inputInt("\nEnter the sample count:\n", greaterThan=0)
+request.batchId = pyip.inputInt("\nEnter the batch ID:\n", greaterThan=0)
+request.batchCount = pyip.inputInt("\nEnter the batch count:\n", greaterThan=0)
+request.dataType = inputMenuWrapper("\nEnter data type:\n", pbtypes.DataType)
+request.analyticsType = inputMenuWrapper("\nEnter the analytics type:\n", pbtypes.AnalyticsType)
+isDataJSON = pyip.inputMenu(prompt="\nWhat should be the format of the request body?\n", choices=["JSON", "Protobuf"]) == "JSON"
+
+data = MessageToJson(request, including_default_value_fields=True) if isDataJSON else request.SerializeToString()
+print("\nResulting request body:\n" + str(data))
+
+headers = {
+  "Content-Type": "application/json" if isDataJSON else "application/x-protobuf" 
+}
+RFD = None
+response = requests.post(url="http://127.0.0.1:8080/", data=data, headers=headers)
+print("\nReceived response [" + str(response.status_code) + "] back...")
+
+if response.status_code == 200:
+  print("Response content:\n" + str(response.content))
+  if response.headers["Content-Type"] == 'application/json':
+    data = response.content
+    RFD = JSONParse(text=data, message=pbtypes.RFD())
+  else:
+    data = response.content
+    RFD = pbtypes.RFD()
+    RFD.ParseFromString(data)
+    
+  print("\nRFD:")
+  print("ID: " + str(RFD.Id))
+  print("Last batch ID: " + str(RFD.lastBatchId))
+  print("Samples: " + str(RFD.samples))
+  print("Analytics: " + str(RFD.analyticsValue))
---
+++ server.py
@@ -0,0 +1,100 @@
+import csv
+import statistics
+
+import numpy as np
+from flask import Flask, request, Response
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import Parse as JSONParse
+
+import types_pb2 as pbtypes
+
+def loadCSVDataset(path):
+  with open(path, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    series = [[], [], [], []]
+    skippedHeader = False
+    for row in reader:
+      if skippedHeader == False:
+        skippedHeader = True
+        continue
+      series[0].append(float(row[0]))
+      series[1].append(float(row[1]))
+      series[2].append(float(row[2]))
+      series[3].append(float(row[3]))
+    return series
+
+DVDSTORE_TRAINING = loadCSVDataset("DVD-training.csv")
+DVDSTORE_TESTING = loadCSVDataset("DVD-testing.csv")
+NDBENCH_TRAINING = loadCSVDataset("NDBench-training.csv")
+NDBENCH_TESTING = loadCSVDataset("NDBench-testing.csv")
+
+app = Flask(__name__)
+
+def calculateAnalytics(samples, totalSampleCount, analyticsType):
+  sum = 0
+  for sample in samples:
+    sum = sum + sample
+  average = sum / totalSampleCount
+  sorted_samples = sorted(samples, key=float)
+  min = sorted_samples[0]
+  max = sorted_samples[totalSampleCount - 1]
+  np_samples = np.array(samples)
+  if analyticsType == pbtypes.AnalyticsType.T10P:
+    return np.percentile(np_samples, 10)
+  elif analyticsType == pbtypes.AnalyticsType.T50P:
+    return np.percentile(np_samples, 50)
+  elif analyticsType == pbtypes.AnalyticsType.T95P:
+    return np.percentile(np_samples, 95)
+  elif analyticsType == pbtypes.AnalyticsType.T99P:
+    return np.percentile(np_samples, 99)
+  elif analyticsType == pbtypes.AnalyticsType.AVG:
+    return average
+  elif analyticsType == pbtypes.AnalyticsType.STD:
+    return statistics.stdev(sample)
+  elif analyticsType == pbtypes.AnalyticsType.MAX:
+    return max
+  elif analyticsType == pbtypes.AnalyticsType.MIN:
+    return min
+
+def generateRFD(RFW):
+  RFD = pbtypes.RFD()
+  RFD.Id = RFW.Id
+  RFD.lastBatchId = RFW.batchId + RFW.batchCount - 1
+  
+  series = None
+  if RFW.benchmarkType == pbtypes.BenchmarkType.DVDSTORE and RFW.dataType == pbtypes.DataType.TRAINING:
+    series = DVDSTORE_TRAINING
+  elif RFW.benchmarkType == pbtypes.BenchmarkType.DVDSTORE and RFW.dataType == pbtypes.DataType.TESTING:
+    series = DVDSTORE_TESTING
+  elif RFW.benchmarkType == pbtypes.BenchmarkType.NDBENCH and RFW.dataType == pbtypes.DataType.TRAINING:
+    series = NDBENCH_TRAINING
+  elif RFW.benchmarkType == pbtypes.BenchmarkType.NDBENCH and RFW.dataType == pbtypes.DataType.TESTING:
+    series = NDBENCH_TESTING
+  serie = series[RFW.workloadMetric]
+
+  # TO-DO: Work on validating the boundaries of the series
+  startingOffset = RFW.batchId * RFW.sampleCount
+  totalSampleCount = RFW.batchCount * RFW.sampleCount
+  samples = serie[startingOffset:startingOffset+totalSampleCount]
+  RFD.samples.extend(samples)
+  RFD.analyticsValue = calculateAnalytics(samples, totalSampleCount, RFW.analyticsType)
+  return RFD
+
+@app.route("/", methods=["POST"])
+def index():
+  RFW = None
+  isJSON = request.mimetype == "application/json"
+  if isJSON:
+    data = request.get_data()
+    RFW = JSONParse(text=data, message=pbtypes.RFW())
+  else:
+    data = request.get_data()
+    RFW = pbtypes.RFW()
+    RFW.ParseFromString(data)
+  RFD = generateRFD(RFW)
+  response = Response(MessageToJson(RFD, including_default_value_fields=True) if isJSON else RFD.SerializeToString())
+  response.headers["Content-Type"] = request.mimetype
+  response.status = 200
+  return response
+
+app.run(host='127.0.0.1', port=8080)
